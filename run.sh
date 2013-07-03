5e2b12e0190e30e806525773f0b85a5a74e9c8b7
wwwouter <wouter@wercker.com>
2013-07-03 15:01:03 +0200

    wrong prefix

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
-if [ ! -n "$WOUTER_HIPCHAT_NOTIFY_TOKEN" ]; then
+if [ ! -n "$WERCKER_HIPCHAT_NOTIFY_TOKEN" ]; then
   error 'Please specify token property'
   exit 1
 fi
 
-if [ ! -n "$WOUTER_HIPCHAT_NOTIFY_ROOM_ID" ]; then
+if [ ! -n "$WERCKER_HIPCHAT_NOTIFY_ROOM_ID" ]; then
   error 'Please specify room_id property'
   exit 1
 fi
 
-if [ ! -n "$WOUTER_HIPCHAT_NOTIFY_FROM_NAME" ]; then
+if [ ! -n "$WERCKER_HIPCHAT_NOTIFY_FROM_NAME" ]; then
   error 'Please specify from_name property'
   exit 1
 fi
 
-if [ ! -n "$WOUTER_HIPCHAT_NOTIFY_MESSAGE" ]; then
+if [ ! -n "$WERCKER_HIPCHAT_NOTIFY_MESSAGE" ]; then
   error 'Please specify message property'
   exit 1
 fi
 
-python "$WERCKER_STEP_ROOT/main.py" $WOUTER_HIPCHAT_NOTIFY_TOKEN $WOUTER_HIPCHAT_NOTIFY_ROOM_ID $WOUTER_HIPCHAT_NOTIFY_FROM_NAME $WOUTER_HIPCHAT_NOTIFY_MESSAGE
+python "$WERCKER_STEP_ROOT/main.py" $WERCKER_HIPCHAT_NOTIFY_TOKEN $WERCKER_HIPCHAT_NOTIFY_ROOM_ID $WERCKER_HIPCHAT_NOTIFY_FROM_NAME $WERCKER_HIPCHAT_NOTIFY_MESSAGE
